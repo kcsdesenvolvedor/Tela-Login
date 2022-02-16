401d2e26efec11ac63c260f377e430ebfebd876d
kcsdesenvolvedor <kcs.desenvolvedor@gmail.com>
2022-02-16 00:32:46 -0300

    implementacao das validações

--- Tela-Login/src/app/app.component.ts
+++ Tela-Login/src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tela-Login';
+  senha = '';
+  disabledButton = true;
+
+
+  verificarNovaSenha(event: any){
+
+    let novaSenha = event.target.value;
+    const validacaoNovaSenha = document.getElementById('novaSenha');
+
+    if(novaSenha.length < 8){
+      validacaoNovaSenha.innerText = 'A nova senha tem que conter no minimo 8 caracter';
+    }else if(novaSenha.length >= 8 && novaSenha.toLowerCase() == novaSenha){
+      validacaoNovaSenha.innerText = 'A nova senha precisa conter um letra Maiúscula';
+    }else if(novaSenha.includes('@') || novaSenha.includes('#') || novaSenha.includes('$') || novaSenha.includes('&') || novaSenha.includes('-')){
+      validacaoNovaSenha.innerText = '';
+    }else{
+      validacaoNovaSenha.innerText = 'A nova senha precisa conter pelo menos um caracter especial: @#$&-';
+    }
+
+    this.senha = novaSenha;
+  }
+
+  verificarRepetirSenha(event: any){
+    let repetirSenha = event.target.value;
+    const validacaoRepetirSenha = document.getElementById('repetirSenha');
+
+    if(repetirSenha != this.senha){
+      validacaoRepetirSenha.innerText = 'A senha repetida está incorreta!';
+    }else{
+      validacaoRepetirSenha.innerText = '';
+      this.disabledButton = false;
+    }
+  }
 }
