e3b28ceacf07382f08f5724ee7ab94e94bd82db6
kcsdesenvolvedor <kcs.desenvolvedor@gmail.com>
2022-02-17 01:02:32 -0300

    commit final

--- Tela-Login/src/app/app.component.ts
+++ Tela-Login/src/app/app.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tela-Login';
   senha = '';
   disabledButton = true;
+  showModal = false;
+  mostrar: boolean = false;
+  closeResult = '';
+  titleResult = '';
+  textResult = '';
+  meuForm: FormGroup;
 
+  constructor(private modalService: NgbModal, private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.createForm();
+    this.meuForm.reset();
+  }
 
   verificarNovaSenha(event: any){
 
@@ -35,9 +49,54 @@ export class AppComponent {
 
     if(repetirSenha != this.senha){
       validacaoRepetirSenha.innerText = 'A senha repetida está incorreta!';
+      this.disabledButton = true;
     }else{
       validacaoRepetirSenha.innerText = '';
       this.disabledButton = false;
     }
+  }
+
+  createForm(){
+    this.meuForm = this.formBuilder.group({
+      senhaAntiga: [''],
+      novaSenha: [''],
+      repetirSenha: ['']
+    })
+  }
+
+  open(content) {
+    const senhaAntiga = this.meuForm.controls['senhaAntiga'].value;
+    
+    
+    if(senhaAntiga != '12345678'){
+      this.titleResult = 'Acesso Negado';
+      this.textResult = `A senha antiga que você informou não coincide com a senha armazenada na nossa base de dados!`;
+    }else{
+      this.titleResult = 'Sucesso'
+      this.textResult = 'Parabens!\n Senha alterada com sucesso.'
+    }
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+      this.meuForm.reset();
+      this.disabledButton = true;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      this.meuForm.reset();
+      this.disabledButton = true;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  toggle () {
+    this.mostrar = !this.mostrar;
   }
 }
